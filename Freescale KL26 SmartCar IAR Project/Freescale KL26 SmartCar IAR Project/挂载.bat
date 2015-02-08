16f73cff98f6d616b926b22bfb233908ada11509
feilong <614183595@qq.com>
2015-02-08 19:02:09 +0800

    新建sourse insight项目

---
+++ Freescale KL26 SmartCar IAR Project/Freescale KL26 SmartCar IAR Project/挂载.bat
@@ -0,0 +1,6 @@
+@echo off
+
+subst y: /d
+
+subst y: "%cd%"
+
---
+++ Freescale KL26 SmartCar IAR Project/Freescale KL26 SmartCar IAR Project/连接.bat
@@ -0,0 +1,15 @@
+@echo off
+
+subst y: /d
+
+subst y: "E:\Freescale KL26SmartCar V2.0\Freescale KL26 SmartCar IAR Project\Freescale KL26 SmartCar IAR Project"
+
+y:
+
+md z:\freescaleV2prjtmp
+
+xcopy Prj_Local z:\freescaleV2prjtmp /E /C /I /F /H /R /Y
+
+mklink /d prj z:\freescaleV2prjtmp
+
+pause
