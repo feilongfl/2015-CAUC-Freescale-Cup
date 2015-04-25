badb673a901b366a114c88ef8cd524a8c8563371
feilong <614183595@qq.com>
2015-04-25 17:18:23 +0800

    删除部分nrf代码

--- Freescale KL26 SmartCar IAR Project/Freescale KL26 SmartCar IAR Project/FLLib/Inc/fl_nrf24l01.h
+++ Freescale KL26 SmartCar IAR Project/Freescale KL26 SmartCar IAR Project/FLLib/Inc/fl_nrf24l01.h
@@ -1,7 +1,7 @@
 #include "common.h"
 #include "fl_cfg.h"
 #include "VCAN_NRF24L0.h"
-#include "VCAN_NRF24L0_MSG.h"
+//#include "VCAN_NRF24L0_MSG.h"
 #include "fl_state.h"
 
 #ifndef _FL_nrf24l01_
@@ -17,12 +17,15 @@ typedef enum
 
 #define Nrf_This Nrf_AT_Car_1//定义当前模块位置
 #define Nrf_DataLen 128
+#define Nrf_CommandLen 2
 
 typedef enum 
 {
 	Nrf_AllGreen,
 	Nrf_NotOnLine,
 	Nrf_SendFail,
+	Nrf_ResultRxValid,
+	Nrf_ResultRxNo,
 
 	Nrf_Others
 }NrfErrorType_e;
@@ -30,9 +33,11 @@ typedef enum
 typedef enum 
 {
 	Nrf_CheckLink,//发送checklink回复checklink
-	
+	Nrf_Ctrl,//遥控
+	Nrf_Set,//设置
+	Nrf_State,//状态
 
-	Nfr_CommandMax
+	Nrf_CommandMax
 }NrfCommand_e;
 
 #define NrfInitRetryTime 20//重试次数
--- Freescale KL26 SmartCar IAR Project/Freescale KL26 SmartCar IAR Project/FLLib/Src/fl_nrf24l01.c
+++ Freescale KL26 SmartCar IAR Project/Freescale KL26 SmartCar IAR Project/FLLib/Src/fl_nrf24l01.c
@@ -2,11 +2,13 @@
 
 
 
-uint8  nrf_rx_buff[Nrf_DataLen + 2 * COM_LEN + DATA_PACKET];         //预多
-uint8  nrf_tx_buff[Nrf_DataLen + 2 * COM_LEN + DATA_PACKET];         //预多
-const uint32 nrf_com_size[COM_MAX] = { 0,//checklink
-
-										0 };//保留
+uint8  nrf_rx_buff[Nrf_DataLen + 2 * Nrf_CommandLen + DATA_PACKET];         //预多
+uint8  nrf_tx_buff[Nrf_DataLen + 2 * Nrf_CommandLen + DATA_PACKET];         //预多
+const uint32 nrf_com_size[Nrf_CommandMax] = { 0,//checklink
+2,//控制
+4,//设置
+sizeof(struct FreescaleCarState)//状态
+										 };
 
 NrfErrorType_e NrfInit()
 {
@@ -19,54 +21,6 @@ NrfErrorType_e NrfInit()
 			return Nrf_NotOnLine;
 		}
 	}
+	nrf_msg_init();//初始化消息
 	return Nrf_AllGreen;
-}
-
-NrfErrorType_e NrfSendStr(char * str,uint32 len)
-{
-	if (nrf_tx(str, DATA_PACKET) == 1)         //发送一个数据包：buff（包为32字节）
-	{
-		//等待发送过程中，此处可以加入处理任务
-
-		while (nrf_tx_state() == NRF_TXING);         //等待发送完成
-
-		if (NRF_TX_OK == nrf_tx_state())
-		{
-			return Nrf_AllGreen;
-		}
-		else
-		{
-			return Nrf_SendFail;
-		}
-	}
-	else
-	{
-		return Nrf_SendFail;
-	}
-}
-
-NrfErrorType_e NrfSendMsg(NrfCommand_e * com, uint8 * reBuff)
-{
-
-}
-
-NrfErrorType_e NrfReceive()
-{
-	NrfCommand_e com;
-	NrfErrorType_e nrf_result;
-	do
-	{
-		nrf_result = nrf_msg_rx(&com, nrf_rx_buff);
-		if (nrf_result == NRF_RESULT_RX_VALID)
-		{
-			switch (com)
-			{
-			case Nrf_CheckLink:
-				NrfSendMsg(&com, nrf_tx_buff);
-				break;
-			default:
-				break;
-			}
-		}
-	} while (nrf_result != NRF_RESULT_RX_NO);         //接收不到数据 才退出
 }
