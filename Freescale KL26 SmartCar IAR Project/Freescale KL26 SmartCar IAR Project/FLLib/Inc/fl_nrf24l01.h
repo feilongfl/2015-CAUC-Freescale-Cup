1fbe9fac788839915019ee1a8caa71060c907a1d
feilong <614183595@qq.com>
2015-04-25 15:47:46 +0800

    修改nrf程序

--- Freescale KL26 SmartCar IAR Project/Freescale KL26 SmartCar IAR Project/FLLib/Inc/fl_nrf24l01.h
+++ Freescale KL26 SmartCar IAR Project/Freescale KL26 SmartCar IAR Project/FLLib/Inc/fl_nrf24l01.h
@@ -1,16 +1,43 @@
 #include "common.h"
 #include "fl_cfg.h"
 #include "VCAN_NRF24L0.h"
+#include "fl_state.h"
 
 #ifndef _FL_nrf24l01_
 #define _FL_nrf24l01_
 
 typedef enum 
 {
+	Nrf_AT_Car_1,
+	Nrf_AT_Car_2,
+
+	Nrf_AT_RC
+}NrfLocal_e;//枚举无线模块位置
+
+#define Nrf_This Nrf_AT_Car_1//定义当前模块位置
+
+
+typedef enum 
+{
 	Nrf_AllGreen,
 	Nrf_NotOnLine,
+	Nrf_SendFail,
 
 	Nrf_Others
 }NrfErrorType_e;
+
+typedef enum 
+{
+	Nrf_CheckLink,
+	
+
+	Nfr_CommandMax
+}NrfCommand;
+
+#define NrfInitRetryTime 20//重试次数
+
+
+NrfErrorType_e NrfInit();//初始化
+NrfErrorType_e NrfSendStr(char * str, uint32 len);//发送字符串
 
 #endif//_FL_nrf24l01+_
--- Freescale KL26 SmartCar IAR Project/Freescale KL26 SmartCar IAR Project/FLLib/Src/fl_nrf24l01.c
+++ Freescale KL26 SmartCar IAR Project/Freescale KL26 SmartCar IAR Project/FLLib/Src/fl_nrf24l01.c
@@ -1,2 +1,39 @@
 #include "fl_nrf24l01.h"
 
+NrfErrorType_e NrfInit()
+{
+	uint8 i = NrfInitRetryTime;
+	while (!nrf_init())                  //初始化NRF24L01+ ,等待初始化成功为止
+	{
+		if (i-- == 0)
+		{
+			ASSERT(true);
+			return Nrf_NotOnLine;
+		}
+	}
+	return Nrf_AllGreen;
+}
+
+NrfErrorType_e NrfSendStr(char * str,uint32 len)
+{
+	if (nrf_tx(str, DATA_PACKET) == 1)         //发送一个数据包：buff（包为32字节）
+	{
+		//等待发送过程中，此处可以加入处理任务
+
+		while (nrf_tx_state() == NRF_TXING);         //等待发送完成
+
+		if (NRF_TX_OK == nrf_tx_state())
+		{
+			return Nrf_AllGreen;
+		}
+		else
+		{
+			return Nrf_SendFail;
+		}
+	}
+	else
+	{
+		return Nrf_SendFail;
+	}
+}
+
