b2765b53d2604e8d44d1d75ce73ae2867221e23e
feilong <614183595@qq.com>
2015-07-24 17:57:38 +0800

    revert

--- Freescale KL26 SmartCar IAR Project/Freescale KL26 SmartCar IAR Project/FLLib/Src/fl_config.c
+++ Freescale KL26 SmartCar IAR Project/Freescale KL26 SmartCar IAR Project/FLLib/Src/fl_config.c
@@ -209,6 +209,8 @@ ConfigErrorType_s ConfigSetDefaultInEeprom()
 	config.Config.AdcNormalMax.Adc.FlAdc1 = 150u;
 	config.Config.AdcNormalMax.Adc.FLAdc2 = 150u;
 	config.Config.AdcNormalMax.Adc.FLAdc3 = 150u;
+	config.Config.AdcNormalMax.Adc.AdcVertical.Adc0 = 150u;
+	config.Config.AdcNormalMax.Adc.AdcVertical.Adc1 = 150u;
 
 
 	config.Config.Motor.Speed.Acturally = 0;
